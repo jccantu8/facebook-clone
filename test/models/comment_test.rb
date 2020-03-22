941da5ebcfbdd4f0f71a93e63e14c644810197c9
Julio Cantu <julio.c.cantu03@utrgv.edu>
2020-03-21 23:42:09 -0500

    Add validations and test for other models

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:John)
+    @post = posts(:ExamplePost)
+    @post.user_id = @user.id
+    @comment = Comment.new(:content => "example comment", user_id: @user.id, post_id: @post.id)
+  end
+
+  test "comment should be valid" do
+    assert @comment.valid?
+  end
+
+  test "comment content should be present" do
+    @comment.content = ''
+    assert_not @comment.valid?
+  end
+
+  test "comment content should not be too long" do
+    @comment.content = 'a' * 10001
+    assert_not @comment.valid?
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:John)
+    @post = Post.new(:title => "mytitle", :content => "example content", user_id: @user.id)
+  end
+
+  test "post should be valid" do
+    assert @post.valid?
+  end
+
+  test "title should be present" do
+    @post.title = ''
+    assert_not @post.valid?
+  end
+
+  test "title should not be too long" do
+    @post.title = 'a' * 256
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = ''
+    assert_not @post.valid?
+  end
+
+  test "content should not be too long" do
+    @post.content = 'a' * 10001
+    assert_not @post.valid?
+  end
 end
