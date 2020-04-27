bb620e2632b035caf7a20864dbc13f333c6cc2b0
Julio Cantu <julio.c.cantu03@utrgv.edu>
2020-04-26 22:55:19 -0500

    message

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+USERS = ["Mary", "John", "Jim", "Anne", "Robert"]
+
+USERS.each do |x|
+    u = User.create(:name => x, :email => "#{x}@example.com", :password => "foobar", :password_confirmation => "foobar")
+
+    (rand(4) + 1).times do
+        u.posts.create(:title => "Here is a title", :content => "And here is a lot of content.")
+    end
+end
+
+USERS.each do |x|
+    u = User.find_by(:name => x)
+
+    (rand(4) + 1).times do
+        u.comments.create(:content => "This is a random comment", :post_id => rand(1..Post.count))
+    end
+
+
+    idArr = []
+
+    u.posts.each do |x|
+        idArr.push(x.id)
+    end
+
+    (rand(4) + 1).times do
+        randomPostId = rand(1..Post.count)
+
+        if idArr.include?(randomPostId)
+            
+        else
+            u.likes.create(:post_id => randomPostId)
+        end
+    end
+
+
+    (rand(2) + 1).times do
+        randomUserId = nil
+
+        loop do
+
+            randomUserId = rand(1..User.count)
+
+        break if (u.id != randomUserId)
+
+        end
+
+        u.friends.create(:friend_id => randomUserId)
+        User.find_by(:id => randomUserId).friends.create(:friend_id => u.id)
+    end
+
+end
+
+USERS.each do |x|
+    u = User.find_by(:name => x)
+
+    1.times do
+        idArr = [u.id]
+
+        u.friends.each do |x|
+            idArr.push(x.friend_id)
+        end
+
+        randomUserId = nil
+
+        loop do
+
+            randomUserId = rand(1..User.count)
+
+        break if !idArr.include?(randomUserId)
+
+        end
+
+        u.sent_friend_requests.create(:requestee_id => randomUserId)
+    end
+end
