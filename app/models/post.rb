a40c2a4beaa89f4024d43ca7f98a1d9fff8bb38b
Julio Cantu <julio.c.cantu03@utrgv.edu>
2020-05-07 04:06:51 -0500

    Add ability to add pictures to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
     has_many :likes
+    has_one_attached :picture
 
     accepts_nested_attributes_for :comments, allow_destroy: true
 
